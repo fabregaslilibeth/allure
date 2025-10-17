d956a7e28530836e3f01ddbc215f0c1c962ccb36
Beth Fabregas <lilibeth.fabregas@lkg.com.au>
2025-10-17 11:58:12 +0800

    service

--- src/data/services.ts
+++ src/data/services.ts
@@ -34,7 +34,7 @@ export const services = [
           },
           {
             title: "30 minutes or less for a full face treatment*",
-            description: "Description 2",
+            description: "Skin Rejuvenation is a non-invasive treatment that uses laser energy to stimulate collagen production and improve the appearance of the skin.",
           },
           {
             title: "Safe For All Skin Types",
